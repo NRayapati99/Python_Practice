4d2c115e2a32a68e6c2d6cdd9d391787bfca3636
NRayapati99 <rayapatinaveen88@gmail.com>
2025-01-20 19:13:25 -0600

    Adding More new files

--- Find The maximum.py
+++ Find The maximum.py
@@ -1,9 +1,12 @@
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 c = int(input("Enter third number: "))
-if a > b > c:
+
+print(f"The Biggest Number is {max(a,b,c)}")
+
+""" if a > b > c:
     print (f"The number {a} is the biggest of Given Numbers")
 elif b > c:
     print(f"The number {b} is the biggest of Given Numbers")
 else:
-    print(f"The number {c} is the biggest of Given Numbers")
+    print(f"The number {c} is the biggest of Given Numbers") """
--- Vowel or Consonant.py
+++ Vowel or Consonant.py
@@ -1,1 +1,5 @@
-letter = chr(input("enter a letter  from A-Z:"))
+letter = str(input("enter a letter  from A-Z:"))
+if letter in {"a","e","i","o","u"}:
+    print(f"The Given Letter {letter} is a Vowel")
+else:
+    print(f"The Given letter {letter} is a Consonant")
